c367bc258b49a9689caab3abbc5ef494999627fa
Emmanuel rapoo <aniemmanuelrapo@gmail.com>
2021-11-23 02:57:57 +0100

    mongoose hooks and creating login, signup pages and password hashing

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Schema } = require('mongoose');
 const mongoose = require('mongoose');
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchamer = new mongoose.Schema({
     email: {
@@ -16,6 +17,19 @@ const userSchamer = new mongoose.Schema({
         minlength: [6, 'Minimum password length is 6 characters']
     },
 });
+
+//fire a function after doc saved to db
+userSchamer.post('save', function(doc, next) {
+    console.log('new user was created & saved', doc);
+    next()
+})
+
+//fire a function before doc saved to db
+userSchamer.pre('save', async function(next){
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+})
 
 const User = mongoose.model('user', userSchamer);
 module.exports = User;
